e7c38765fc59ec565accb4a50e4c2e74e6f54d36
marinu96 <marinu96.mne@gmail.com>
2023-02-22 18:10:38 +0100

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import './App.css';
+import "./App.css";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
+import { IconContext } from "react-icons";
 
 function App() {
   return (
-    <div>
-      
+    <div className="component">
       <h2>Playing Now</h2>
-      <img src="../public/music/zitti-e-buoni.jpg" alt="Zitti e Buoni - Maneskin" width="200" height="200" />
+      <img
+        className="musicCover"
+        src="https://cdn.smehost.net/sonymusices-45pressprod/wp-content/uploads/2021/05/8bdf8aa0-97cf-4e20-88c9-817fb7b6327a.jpg"
+        alt="Zitti e Buoni - Maneskin"
+        width="200"
+        height="200"
+      />
 
       <div>
-        <h3>Maneskin</h3>
-        <p>Zitti e Buoni</p>
+        <h3 className="title">Maneskin</h3>
+        <p className="subTitle">Zitti e Buoni</p>
       </div>
 
       <div>
-        <button>Back</button>
-        <button>Play</button>
-        <button>Forward</button>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#9a2929" }}>
+            <BiSkipPrevious />
+          </IconContext.Provider>
+        </button>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#9a2929" }}>
+            <AiFillPlayCircle />
+          </IconContext.Provider>
+        </button>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#9a2929" }}>
+            <BiSkipNext />
+          </IconContext.Provider>
+        </button>
       </div>
-
     </div>
   );
 }
